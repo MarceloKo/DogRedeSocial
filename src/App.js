a8cfca97c9b1f54e77f51266f88600c69f92ee81
Marcelo <marceloakohlhase@gmail.com>
2022-01-04 11:48:13 -0400

    Criando rotas do Header

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css'
 import Footer from './Components/Footer';
 import Header from './Components/Header';
+import Home from './Components/Home';
+import Login from './Components/Login/Login';
 
 function App() {
   return (
     <div>
-      <Header/>
-      <Footer/>
+      <BrowserRouter>
+        <Header/>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/login" element={<Login/>}/>
+        
+        </Routes>
+        <Footer/>
+      </BrowserRouter>
+      
+      
     </div>
   );
 }
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,10 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import styles from './Header.module.css'
 
 const Header = () => {
     return (
         <div className={styles.header}>
-            Header
+           <nav className='container'>
+               <Link to="/">Home</Link>
+               <Link to="/login">Login / Criar</Link>
+           </nav>
         </div>
     )
 }
