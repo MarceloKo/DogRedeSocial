d1a090f7086c4444353ca4dcc7796604adc6c386
Marcelo <marceloakohlhase@gmail.com>
2022-01-04 10:35:01 -0400

    Criando os componentes Header e Footer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
+
+import './App.css'
+import Footer from './Components/Footer';
+import Header from './Components/Header';
+
 function App() {
   return (
-    <div>App react</div>
+    <div>
+      <Header/>
+      <Footer/>
+    </div>
   );
 }
 
