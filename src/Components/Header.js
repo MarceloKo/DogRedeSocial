c2a51f2291e7915a93082ab03812712529deaa6a
Marcelo <marceloakohlhase@gmail.com>
2022-01-05 13:05:14 -0400

    Passando dados da API do usuário para o HEADER

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,16 +2,23 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import styles from './Header.module.css'
 import {ReactComponent as Dogs} from '../Assets/dogs.svg'
+import { UserContext } from '../UserContext'
 
 const Header = () => {
+    const {data} = React.useContext(UserContext)
+    
     return (
         <header className={styles.header}>
            <nav className={`${styles.nav} container`}>
                <Link className={styles.logo} to="/" aria-label='Dogs - Home'>
                    <Dogs/>
                </Link>
-               
-               <Link to="/login" className={styles.login}>Login / Criar</Link>
+               {data ? (
+               <Link to="/conta" className={styles.login}>{data.nome}</Link>
+                  ): (
+                  
+                <Link to="/login" className={styles.login}>Login / Criar</Link>)}
+              
            </nav>
         </header>
     )
--- src/Components/Login/LoginForm.js
+++ src/Components/Login/LoginForm.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import { TOKEN_POST, USER_GET} from '../../api'
 import useForm from '../../Hooks/useForm'
+import { UserContext } from '../../UserContext'
 import Button from '../Forms/Button'
 import Input from '../Forms/Input'
 
@@ -9,33 +9,14 @@ import Input from '../Forms/Input'
 const LoginForm = () => {
     const username = useForm();
     const password = useForm();
-   
-    React.useEffect(()=>{
-        const token = window.localStorage.getItem('token');
-        if(token){
-            getUser(token)
-        }
-    })
-
-
-
-    async function getUser(token){
-        const {url,options} = USER_GET(token)
-        const response = await fetch(url,options)
-        const json = await response.json();
-        console.log(json)
-    }
-
+  
+    const {userLogin} = React.useContext(UserContext)
 
    async function handleSubmit(event){
         event.preventDefault();
 
         if(password.validate() && username.validate()){
-        const{url,options} = TOKEN_POST({username:username.value,password:password.value})
-        const response= await fetch(url,options)
-        const json = await response.json();
-        window.localStorage.setItem('token',json.token)
-        getUser(json.token)
+            userLogin(username.value,password.value)
         }
     }
 
