cfb16c33f81feb140284ea174473b276af70660b
Marcelo <marceloakohlhase@gmail.com>
2022-01-06 10:28:50 -0400

    Protegendo rota do usuario cadastrado e logado

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css'
 import Footer from './Components/Footer';
 import Header from './Components/Header';
+import ProtectedRouter from './Components/Helper/ProtectedRouter';
 import Home from './Components/Home';
 import Login from './Components/Login/Login';
+import User from './Components/User/User';
 import { UserStorage } from './UserContext';
 
 function App() {
@@ -15,7 +17,8 @@ function App() {
           <Header/>
           <Routes>
             <Route path="/" element={<Home/>}/>
-            <Route path="/login/*" element={<Login/>}/>
+            <Route path="login/*" element={<Login/>}/>
+            <Route path="conta/*" element={<ProtectedRouter><User/></ProtectedRouter>}/>
           
           </Routes>
           <Footer/>
---
+++ src/Components/Helper/ProtectedRouter.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Navigate } from 'react-router-dom'
+import { UserContext } from '../../UserContext'
+
+const ProtectedRouter = ({children}) => {
+    const {login} = React.useContext(UserContext)
+    return login? children: <Navigate to='/login' />
+}
+
+export default ProtectedRouter
