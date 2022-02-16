282767f35140ccdc9433d7ac88936e1d6d38d686
MarceloKo <marceloakohlhase@gmail.com>
2022-02-15 21:59:11 -0400

    Recuperar senha

--- src/Components/Login/LoginPasswordLost.js
+++ src/Components/Login/LoginPasswordLost.js
@@ -1,10 +1,41 @@
 import React from 'react'
-
+import Input from '../Forms/Input'
+import Button from '../Forms/Button'
+import useForm from '../../Hooks/useForm'
+import useFetch from '../../Hooks/useFetch'
+import { PASSWORD_LOST } from '../../api'
+import Error from '../Helper/Error'
+import Head from '../Helper/Head'
 const LoginPasswordLost = () => {
+    
+    const login = useForm()
+    const{data,loading,error,request}=useFetch()
+    
+
+    async function handleSubmit (event){
+        event.preventDefault();
+        if(login.validate()){
+            const{url,options}= PASSWORD_LOST({
+                login:login.value,
+                url:window.location.href.replace('perdeu','resetar')
+            })
+            request(url,options)
+        }
+      
+    }
     return (
-        <div>
-            
-        </div>
+        <section>
+             <Head title='Perdeu a senha' />
+
+            <h1 className='title'>Perdeu a senha?</h1>
+            {data ? <p style={{color:'#4c1'}}>{data}</p>: 
+            <form onSubmit={handleSubmit}>
+                <Input label='Email / Usuário' text='text' name='login' {...login}/>
+                {loading ?<Button disabled>Enviar Email</Button>:<Button>Enviar Email</Button>}
+            </form>}
+           
+            <Error error={error}/>
+        </section>
     )
 }
 
